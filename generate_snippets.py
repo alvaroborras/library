ffd843be59b13204e5fd13a7123fab9cafe25925
Álvaro Borrás <alvaroborrasf@gmail.com>
2024-05-06 11:41:03 +0200

    Modified generate_snippets.py - now it only looks for scripts in the specified directories. It ignores the tests and .cpp files which are not supposed to be snippets.

--- generate_snippets.py
+++ generate_snippets.py
@@ -1,35 +1,33 @@
-# Generates cpp.json with snippets for VSCode
-
 import os
 import json
 import sys
 
 snippets = {}
 
-for subdir in os.walk('.'):
-  if subdir[0] == '.' or '.git' in subdir[0]:
-    continue
-  for filename in subdir[2]:
-    name, extension = filename.split('.')
-    if extension != 'cpp':
-      continue
+directories = ['string']
 
-    if name in snippets:
-      print(f'error: duplicate snippet {name}', file=sys.stderr)
-      exit(0)
+for directory in directories:
+    for filename in os.listdir(directory):
+        name, extension = filename.split('.')
+        if extension != 'cpp':
+            continue
 
-    path = subdir[0] + '/' + filename
-    with open(path, 'r') as f:
-      snippet = {
-        "prefix": name,
-        "body": [line.rstrip() for line in f.readlines()],
-        "description": name
-      }
-      snippets[name] = snippet
+        if name in snippets:
+            print(f'error: duplicate snippet {name}', file=sys.stderr)
+            exit(0)
 
-    print(f'generated snippet {name}', file=sys.stderr)
+        path = os.path.join(directory, filename)
+        with open(path, 'r') as f:
+            snippet = {
+                "prefix": name,
+                "body": [line.rstrip() for line in f.readlines()],
+                "description": name
+            }
+            snippets[name] = snippet
+
+        print(f'generated snippet {name}', file=sys.stderr)
 
 with open('cpp.json', 'w') as f:
-  f.write(json.dumps(snippets, indent=2))
+    f.write(json.dumps(snippets, indent=2))
 
 print('done', file=sys.stderr)
