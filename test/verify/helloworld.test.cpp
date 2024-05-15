22c9af2b4850c95f351dffea226c68a8ed7558aa
Álvaro Borrás <alvaroborrasf@gmail.com>
2024-05-15 10:48:21 +0200

    add sample tests

---
+++ test/verify/helloworld.test.cpp
@@ -0,0 +1,5 @@
+#define PROBLEM                                                                \
+  "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A"
+#include <bits/stdc++.h>
+
+int main() { std::cout << "Hello World" << std::endl; }
